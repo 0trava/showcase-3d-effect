fadc4495cf51e6f2594b2ba1f1502da8f46ea1ea
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-05-19 16:23:04 +0300

    add swiper slider

--- js/common.js
+++ js/common.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+	const showSlider = new Swiper('.showcase-carousel', {
+		loop: true,
+		slidesPerView: 3,
+		speed: 1800
 
+	})
 
 
 	document.querySelector('video').playbackRate = 2 // Пришвидшуємо роботу відео в 2 рази 
