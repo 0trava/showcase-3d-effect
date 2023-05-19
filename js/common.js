89c431f4cd5451c61277d66d0dd83f0c8209a68f
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-05-19 17:15:19 +0300

    add styles for slider

--- js/common.js
+++ js/common.js
@@ -3,7 +3,12 @@ document.addEventListener('DOMContentLoaded', function() {
 	const showSlider = new Swiper('.showcase-carousel', {
 		loop: true,
 		slidesPerView: 3,
-		speed: 1800
+		speed: 1800,
+		centeredSlides: true,
+		navigation: {
+			nextEl: '.showcase-navigation__next',
+			prevEl: '.showcase-navigation__prev'
+		}
 
 	})
 
